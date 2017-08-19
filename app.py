64d55b40203b13f6d61e1468059514d2e8c6f9b8
bestiatbg <olek.tbg.konta@gmail.com>
2017-08-19 20:35:18 +0200

    Printing loaded data has changed

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
 from data_loader.data_loader import CsvDataLoader
 
+
+def print_input_record(input_record):
+    print("input_record with NIP : {}".format(input_record.nip))
+    print(input_record)
+    
+    print_elements_in_collection("Engagements", input_record.engagements)
+    print_elements_in_collection("Proposals", input_record.proposals)
+    print_elements_in_collection("BDA", input_record.bda)
+    
+    print("=" * 50)
+    
+    
+def print_elements_in_collection(collection_name, collection):
+    print("{} ({}): ".format(collection_name, len(collection)))
+    
+    for elem in collection:
+        print("\t{}".format(elem))
+
 # Check if this is a main thread
 if __name__ == "__main__":
     data_loader = CsvDataLoader()
     data_loader.load_data()
 
-    # Test if data is loaded
+    # Print loaded data
     for nip in data_loader.input_entities:
-        print(data_loader.input_entities[nip])
-        print(data_loader.input_entities[nip].assign_category())
-        print("Engagements : ")
-        for engagement in data_loader.input_entities[nip].engagements:
-            print("\t{}".format(engagement))
-        print("Proposals : ")
-        for proposal in data_loader.input_entities[nip].proposals:
-            print("\t{}".format(proposal))
-        print("bda : ")
-        for bda in data_loader.input_entities[nip].bda:
-            print("\t{}".format(bda))
-        print("=" * 150)
+        print_input_record(data_loader.input_entities[nip])
+
