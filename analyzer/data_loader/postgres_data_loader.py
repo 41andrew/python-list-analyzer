ae12212ad9869eb0ea857e1a663b8780de2848f4
mandrrew <jedrek.malinowski@gmail.com>
2017-08-28 15:21:16 +0200

    Bug fixes

--- analyzer/data_loader/postgres_data_loader.py
+++ analyzer/data_loader/postgres_data_loader.py
@@ -10,7 +10,11 @@ from ..model.campaign import Campaign
 
 class PostGreDataLoader:
 
-    CAMPAIGN_ID_LIST = [35, 36, 40, 48]
+    CAMPAIGN_ID_LIST = [6, 7, 11, 14, 16, 17, 18, 19,
+                        20, 21, 23, 24, 26, 27, 28, 29,
+                        30, 31, 32, 33, 34, 35, 36, 37,
+                        39, 40, 41, 42, 43, 44, 46, 47,
+                        48, 49, 50]
 
     all_campaigns = []
 
